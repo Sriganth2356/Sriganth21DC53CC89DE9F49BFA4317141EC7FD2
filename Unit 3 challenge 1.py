1d57758f3ef1fa3edc72cefb2a2fc4ca6e04211b
Sriganth2356 <144885588+Sriganth2356@users.noreply.github.com>
2023-09-21 13:51:41 +0530

    Create Unit 3 challenge 1.py

---
+++ Unit 3 challenge 1.py
@@ -0,0 +1,17 @@
+def linear_search_product(products, target_product) Indices = []
+
+for 1, product in enumerate(products): If product = target_product:
+
+Indices.append())
+
+return indices
+
+products = ["Apple", "Banana", "Orange". "Apple", "Grapes", "Apple"] target_product = "Apple"
+
+result = linear_search_product(products, target_product) if result: print("The target product (target product)' was found at indices:
+
+(result)")
+
+else:
+
+print("The target product '(target_product)' was not found in the list.")
